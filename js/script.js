3d7566ac49565aa459a65df894e11386893c5ce4
Robert Koszela <dorso123@gmail.com>
2017-12-21 20:32:03 +0100

    zadanie8_5

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// scripts.js
+alert ('Surface area of triangle = a*h/2');
+var a = prompt('Enter a')
+	h = prompt('Enter h')
+	triangleArea =  a*h/2;
+alert('Result = ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
